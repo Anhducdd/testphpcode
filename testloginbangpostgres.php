cb2cb29789af6c5965f0e418a7b803261c11d9b6
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-20 00:04:46 +0700

    Update testloginbangpostgres.php

--- testloginbangpostgres.php
+++ testloginbangpostgres.php
@@ -1,20 +1,28 @@
 <html>
 <head>
+<title>Login</title>
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
-<form action="Connect.php" method="POST" id="formLogin">
+<body class="container">
+<div class="header">
+  	<h2>Login</h2>
+  </div>
+<form method="post" action="Connect.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="user" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="Submit">Login</button>
+  	</div>
+  </form>
 
-            <label>User</label><br/>
-            <input name="user" type="text" id="user" /><br/>
-            <label>Password</label><br/>
-            <input name="password" type="password" id="password" /><br/><br/>
-            <input type="submit" name="Submit" value="LOGIN" />
-
- 
-
-</form>
-<?php
-
-?>
 </body>
 </html>
