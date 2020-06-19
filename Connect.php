258e2bb4070b10678fe40a6fa70af486750db419
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-20 00:05:33 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,17 +1,18 @@
 <?php
+$errors = array();
 $conn_string="host=ec2-34-197-141-7.compute-1.amazonaws.com
  port=5432 dbname=db7lrj93isfe3h user=hlphipxlcbjlwm password=ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f";
 $dbconn= pg_connect($conn_string);
-if (isset($_POST['user'])) {
+if (isset($_POST['username'])) {
 	# code...
-	$username = $_POST['user'];
+	$username = $_POST['username'];
 }
 
-if (isset($_POST['password'])) {
+if (isset($_POST['pass'])) {
 	# code...
-	$pass = $_POST['password'];
+	$pass = $_POST['pass'];
 }
-$sql = "SELECT * FROM myaccount WHERE _username='".$username."' AND _password='".$pass."'";
+$sql = "SELECT * FROM tblAccount WHERE _user='".$username."' AND _pass='".$pass."'";
 $result =pg_query($dbconn, $sql);
 $row = pg_num_rows($result);
 if ($row==1)
@@ -20,6 +21,6 @@ if ($row==1)
 }
 else
 {
-	echo "Login failed";
+	array_push($errors, "Wrong username/password combination");
 }
 ?>
