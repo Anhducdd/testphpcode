1e9c2b4235903266f03c9e00b2bbac4804b6f2f8
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-20 01:16:00 +0700

    Update db.php

--- viewpage/db.php
+++ viewpage/db.php
@@ -1,7 +1,6 @@
 <?php
-$con = mysqli_connect("localhost","root","","tromweb");
-	if (mysqli_connect_errno()){
-		echo "Failed to connect to MySQL: " . mysqli_connect_error();
-		die();
-		}
+$conn_string="host=ec2-34-197-141-7.compute-1.amazonaws.com
+ port=5432 dbname=db7lrj93isfe3h user=hlphipxlcbjlwm password=ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f";
+$dbconn= pg_connect($conn_string);
+
 ?>
