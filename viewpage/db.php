3d55a304a210cb4c3f772ce028d6e178f67b2e98
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-20 20:36:23 +0700

    Update db.php

--- viewpage/db.php
+++ viewpage/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn_string="host=ec2-34-197-141-7.compute-1.amazonaws.com
- port=5432 dbname=db7lrj93isfe3h user=hlphipxlcbjlwm password=ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f";
+$db_connect=pg_connect("host=ec2-34-197-141-7.compute-1.amazonaws.com
+ port=5432 dbname=db7lrj93isfe3h user=hlphipxlcbjlwm password=ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f");
 
 ?>
