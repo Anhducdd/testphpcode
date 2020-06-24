809873d8a11224875b674812e751b109ee65e98f
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-24 23:58:59 +0700

    Update db.php

--- php-1/db.php
+++ php-1/db.php
@@ -1,9 +1,8 @@
 <?php
 try {
     
- $conn_string="host=ec2-34-197-141-7.compute-1.amazonaws.com
- port=5432 dbname=db7lrj93isfe3h user=hlphipxlcbjlwm password=ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f";
-  $db = pg_connect($conn_string);
+
+  $db = new PDO('pgsql:host=ec2-34-197-141-7.compute-1.amazonaws.com;dbname=db7lrj93isfe3h', 'hlphipxlcbjlwm', 'ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f');
   return $db;
 }catch (Exception $e){
     echo "Kết nối ko thành công";
