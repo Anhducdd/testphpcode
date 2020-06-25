5c0ca1339c43620cf969686a85069b0db6094a56
Anh Duc(andy) <53077402+Anhducdd@users.noreply.github.com>
2020-06-25 10:08:42 +0700

    Update and rename db.php to databaseconnect.php

--- php-1/databaseconnect.php
+++ php-1/databaseconnect.php
@@ -5,5 +5,5 @@ try {
   $db = new PDO('pgsql:host=ec2-34-197-141-7.compute-1.amazonaws.com;dbname=db7lrj93isfe3h', 'hlphipxlcbjlwm', 'ea2f7fd249414dbe1e27433c83057cc931dd53a782295ac070c40d29e9d8a94f');
   return $db;
 }catch (Exception $e){
-    echo "Kết nối ko thành công";
+    echo "Fail";
 }
